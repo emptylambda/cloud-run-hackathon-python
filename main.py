d1f220d42011fa3d539f3f1f497c50f8b53ef778
Jeff Chen <jchen@sentons.com>
2022-06-10 11:58:56 +0800

    avoid being milk

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def move():
         return moves[random.randrange(len(moves))]
 
 
-    return 'T' # moves[random.randrange(len(moves))]
+    return  moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
